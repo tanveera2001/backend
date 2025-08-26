0cf7b454d596bf4db4fca342503b299569d924c6
Md. Tanveer Ahmed <tanveera2001@gmail.com>
2025-08-26 19:47:39 +0600

    gfdf

--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,11 @@
-require("dotenv").config();
 const mongoose = require("mongoose");
-
-
+const { mongodbURL } = require("../secret");
 
 
 
 const connectDatabase = async (options) => {
     try {
-        await mongoose.connect(process.env.MONGODB_ATLAS_URL, options);
+        await mongoose.connect(mongodbURL, options);
         console.log("Successfully connected to the MongoDB database.");
         mongoose.connection.on("error", (error) => {
             console.error("Runtime MongoDB connection error:", error.toString());
--- src/secret.js
+++ src/secret.js
@@ -1,4 +1,4 @@
 require("dotenv").config();
 const serverPort = process.env.SERVER_PORT || 3002;
-// const mongodbURL = process.env.MONGODB_ATLAS_URL || "mongodb://localhost:27017/tanveer-portfolio";
-module.exports = { serverPort};
+const mongodbURL = process.env.MONGODB_ATLAS_URL || "mongodb://localhost:27017/tanveer-portfolio";
+module.exports = { serverPort, mongodbURL};
