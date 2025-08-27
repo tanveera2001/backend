dd7a00b32493c1b3d4f3c36fa50cb8d47206126e
Md. Tanveer Ahmed <tanveera2001@gmail.com>
2025-08-27 19:37:35 +0600

    dsdsd

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,8 +1,14 @@
-const cors = require("cors");
+require('dotenv').config(); // We must need this because we are using .env file's variable
+
+const FRONTEND_URL = process.env.FRONTEND_URL;
 
 const corsOptions = {
-  origin: "https://tanveer-ahmed.vercel.app", // your Vercel frontend URL
-  credentials: true,
+  origin: function (origin, callback) {
+    if(!origin) return callback(null, true); // Postman has no origin. So when we request from postman this line allows it.
+    if(origin == FRONTEND_URL) callback(null, true);
+    else callback(new Error("Not allowed by CORS"));
+  },
+  credentials: true, // This is for allowing cookies
 };
 
 module.exports = cors(corsOptions);
